902739c1b1117ce2796deb699f138d467e89b53d
JovanyCm1 <jaime.clasess@gmail.com>
2025-08-09 12:37:33 -0600

    25. Temas puntuales de la sección

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import './style.css'
-import './celan-code/04-homework.ts'
+import "./style.css";
+import "./celan-code/06-cleasses-a.ts";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
-`
-
+`;
